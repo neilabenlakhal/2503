6a88d2beb5a77b790cd5ccf1645a6fa3aeb0a14d
Neila BEN LAKHAL <neila.benlakhal@gmail.com>
2022-11-07 12:17:17 +0400

    Add files via upload

---
+++ Labs/W10/ex7.js
@@ -0,0 +1,15 @@
+const http = require('http') // http built in module
+    //use parameters req and res for our server as we need access to these
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end("Welcome to our home page")
+    }
+    //res is response being sent back
+    if (res.url === '/about') {
+        res.end('Here is our short history')
+    }
+
+    res.end("<h1> oops! </h1><p> cannot find what you are looking for </p><a href = '/'> back home </a>")
+})
+
+server.listen(5000)
