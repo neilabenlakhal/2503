3cdad24cd522fd52b7c84d4f9f1a63091065c8e8
Neila Ben Lakhal <nlakhal@hct.ac.ae>
2022-11-25 12:12:16 +0400

    W12

---
+++ Labs/W11/Ex_5/app.js
@@ -0,0 +1,14 @@
+const express = require('express');  //import the module 
+const path = require('path'); // use the pre-installed path module
+const app = express(); // invoke the express module
+
+//set up static and middleware
+app.use(express.static('./public')) 
+//With express place all static resources into a folder and place copy of resources to public folder
+
+app.get('*',(req,res)=>{
+ res.status(400).send('resource not found')
+})
+app.listen(5000,()=>{
+    console.log('server is listening on port 5000....')
+})
---
+++ Labs/W11/Ex_8/index.js
@@ -0,0 +1,30 @@
+/* to run this code 
+
+npm install validator 
+node index.js */
+
+
+
+const validator = require('validator')
+  // Check whether given email is valid or not
+var email = 'test@gmail.com'
+console.log(validator.isEmail(email))  // true
+email = 'test@'
+console.log(validator.isEmail(email))  // false
+  
+// Check whether string is in lowercase or not
+var name = 'geeksforgeeks'
+console.log(validator.isLowercase(name))  // true
+name = 'GEEKSFORGEEKS'
+console.log(validator.isLowercase(name))  // false
+  
+// Check whether string is empty or not
+var name = ''
+console.log(validator.isEmpty(name))  // true
+name = 'geeksforgeeks'
+console.log(validator.isEmpty(name))  // false
+  
+// Other functions also available in 
+// this module like isBoolean()
+// isCurrency(), isDecimal(), isJSON(),
+// isJWT(), isFloat(), isCreditCard(), etc.
